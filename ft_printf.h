afb5cb65b50025be361019223849e91dcfd06627
Sabri MAKNI <smakni@e3r2p2.42.fr>
2018-06-08 15:39:54 +0200

    add define

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:48:17 by smakni            #+#    #+#             */
-/*   Updated: 2018/06/08 14:50:47 by smakni           ###   ########.fr       */
+/*   Updated: 2018/06/08 15:27:10 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,9 @@
 #include <stdlib.h>
 #include <strarg.h>
 #include <string.h>
+
+#define conv "sSpdDioOuUxXcC"
+#define flag "#0-+ hljz"
+#define dhflag "hh"
+#define dlflag "ll"
 
