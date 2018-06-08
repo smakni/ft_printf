220e8ec1e31e7fb03c591154804b4ac870c31025
Sabri MAKNI <smakni@e3r2p2.42.fr>
2018-06-08 16:15:25 +0200

    add ft_conv

--- ft_printf.c
+++ ft_printf.c
@@ -6,29 +6,56 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:45:26 by smakni            #+#    #+#             */
-/*   Updated: 2018/06/08 15:29:34 by smakni           ###   ########.fr       */
+/*   Updated: 2018/06/08 16:14:21 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void	ft_conv(char c, va_list av)
+{
+	char 	*str;
+	char	l;
+	int		x;
+
+	if (c == 's')
+	{
+		str = va_arg(av, char *);
+		ft_putstr(str);
+	}
+	if (c == 'd')
+	{
+		x = va_arg(av, int);
+		ft_putnbr(x);
+	}
+	if (c = 'c')
+	{
+		l = va_arg(av, char)
+		ft_putchar(l);
+	}
+}
+
+
 int		ft_printf(const char *format, ...)
 {
 	va_list av;
-	size_t 	i;
+	char 	*tmp;
+	int 	i;
 
-	va_start(av, str);
+	tmp = (char *)format;
+	va_start(av, format);
 	i = 0;
-	while (str[i])
+	while (tmp[i])
 	{
-		if (str[i] == '%')
+		if (tmp[i] == '%')
 		{
-			check(str[i + 1]) == flag;
+			i++;
+			ft_conv(tmp[i], av);
 		}
 		else
-			ft_putchar(str[i]);
+			ft_putchar(tmp[i]);
 		i++;
 	}
 	va_end(av);
-	return (len);
+	return (i);
 }
