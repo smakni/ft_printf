024f640dc203128b9c0e3fa6e373d1f4211ad640
Sabri MAKNI <smakni@e3r2p2.42.fr>
2018-06-08 15:11:46 +0200

    start function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:45:26 by smakni            #+#    #+#             */
-/*   Updated: 2018/06/08 14:53:30 by smakni           ###   ########.fr       */
+/*   Updated: 2018/06/08 15:11:24 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int		ft_printf(const char *format, ...)
 {
-	
+	va_list av;
+	size_t 	i;
+
+	va_start(av, str);
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			check(str[i + 1]) == flag;
+		}
+		else
+			ft_putchar(str[i]);
+		i++;
+	}
+	va_end(av);
 }
