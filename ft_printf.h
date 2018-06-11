51e3db84e71e80797240187533bf19c2f75850dd
Sabri Makni <sabrimakni@yahoo.fr>
2018-06-11 23:21:16 +0200

    comment

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:48:17 by smakni            #+#    #+#             */
-/*   Updated: 2018/06/11 20:50:28 by sabri            ###   ########.fr       */
+/*   Updated: 2018/06/11 23:20:21 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@
 #include <string.h>
 
 #define conv 	"sSpdDioOuUxXcC"
-#define flag 	"#0-+ hljz"
-#define dhflag 	"hh"
-#define dlflag 	"ll"
+#define flag 	"#0-+ hhlljz"
 
+// une fonction d'analise de la conversion
+// stockage dans structure
+// fonction de conversion de la structure
+// fonction d'affichage de la conversion
 
 typedef struct s_format
 {
 	char 	*str;
 	int		len;
 	void	*pf;
-
 }				t_format;
-
 
 int		ft_printf(const char *format, ...);
 void	ft_putchar(char c);
