731929119bd9c8ae4ee4934ec88c35eb7051c4f9
Sabri Makni <sabrimakni@yahoo.fr>
2018-06-25 11:40:06 +0200

    cleaning

--- conversion.c
+++ conversion.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/19 15:15:56 by smakni            #+#    #+#             */
-/*   Updated: 2018/06/24 01:35:11 by sabri            ###   ########.fr       */
+/*   Updated: 2018/06/24 01:43:37 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,84 +47,8 @@ void	conversion_d(t_format *arg, va_list av)
 	}
 	ft_strdel(&nb);
 	ft_putstr(arg->res);
+	ft_strdel(&arg->res);
 }
-
-
-
-
-
-
-
-
-/*
-void	conversion_d(t_format *arg, va_list av) //check precisison avant check largeur
-{
-	int			len_nb;
-	char		*nb;
-	int			i;
-
-	i = -1;
-	nb = ft_itoa(va_arg(av, int));
-	len_nb = ft_strlen(nb);
-	if (strchr(arg->str, '.'))
-	{
-		if (arg->width > arg->precision)
-		{
-			arg->res = ft_memalloc(arg->width + 1);
-			while (i++ < len_nb)
-				arg->res[arg->width - len_nb + i] = nb[i];
-			i = 0;
-			while (arg->res[i] != nb[0])
-		}
-		else
-			arg->res = ft_memalloc(arg->precision + 1);
-		ft_memset(arg->res, '0', arg->precision - len_nb);
-		while (i++ < len_nb)
-			arg->res[arg->precision - len_nb + i] = nb[i];
-	}
-	else if (ft_strchr(arg->str, '-') == 0 && arg->width > len_nb)
-	{
-		arg->res = ft_memalloc(arg->width + 1);
-		if (ft_strchr(arg->option, '0') != 0)
-			ft_memset(arg->res, '0', arg->width - len_nb);
-		else if (ft_strlen(arg->option) == 0)
-			ft_memset((arg)->res, ' ', (arg)->width - len_nb);
-		while (i++ < len_nb)
-			arg->res[arg->width - len_nb + i] = nb[i];
-		if (ft_strchr(arg->option, ' ') != 0 && ft_strchr(arg->option, '0') != 0)
-			arg->res[0] = ' ';
-	}
-	else
-		conversion_d2(arg, len_nb, nb);
-	ft_putstr(arg->res);
-	ft_strdel(&nb);
-	//ft_strdel(&(arg)->res);
-}
-
-void	conversion_d2(t_format *arg,int len_nb, char *nb)
-{
-	int i;
-
-	i = 0;
-	if (arg->width > len_nb)
-	{
-		arg->res = ft_memalloc(arg->width + 1);
-		while (arg->res[i] || i < len_nb)
-		{
-			arg->res[i] = nb[i];
-			i++;
-		}
-		while (i < arg->width)
-		{
-			arg->res[i] = ' ';
-			i++;
-		}
-		arg->res[i] = '\0';
-	}
-	else
-		arg->res = nb;
-}
-*/
 
 void	conversion_s(t_format *arg, va_list av)
 {
