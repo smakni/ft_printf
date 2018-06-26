3f710b0d5f72b9fd213e6ecfa26893af87fa0e8c
Sabri Makni <sabrimakni@yahoo.fr>
2018-06-26 23:59:42 +0200

    conv_d fixed

--- conversion_d.c
+++ conversion_d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   conversion_d.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
+/*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 23:12:30 by sabri             #+#    #+#             */
-/*   Updated: 2018/06/26 19:56:27 by smakni           ###   ########.fr       */
+/*   Updated: 2018/06/26 23:56:17 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ void	conversion_d(t_format *arg, va_list av)
 	nb = NULL;
 	nb = conversion_d1(arg, av);
 	len_nb = ft_strlen(nb);
-	if (arg->precision > len_nb)
+	if (arg->precision >= len_nb)
 		nb = conversion_d2(arg, nb);
-	if (arg->width < len_nb && arg->precision < len_nb)
+	if (arg->width <= len_nb)
 		arg->res = ft_strdup(nb);
-	else if (arg->width >= arg->precision)
+	else if (arg->width > arg->precision)
 		conversion_d3(arg, nb, len_nb, i);
 	else if (arg->width < arg->precision)
 		conversion_d4(arg, nb, len_nb, i);
@@ -45,16 +45,14 @@ void	conversion_d(t_format *arg, va_list av)
 char	*conversion_d1(t_format *arg, va_list av)
 {
 	int i;
-	int len_nb;
 	char *tmp;
 	char *nb;
 
 	i = 0;
 	nb = ft_itoa(va_arg(av, int));
-	len_nb = ft_strlen(nb);
 	if (ft_strchr(arg->option, '+') != 0 && nb[0] != '-') 
 	{
-		tmp = ft_memalloc(len_nb + 1);
+		tmp = ft_memalloc(ft_strlen(nb) + 2);
 		tmp[0] = '+';
 		while (nb[i])
 		{
@@ -78,9 +76,10 @@ char 	*conversion_d2(t_format *arg, char *nb)
 		tmp = ft_memalloc(arg->precision + 1);
 		ft_memset(tmp, '0', arg->precision + 1);
 		tmp[0] = nb[0];
+		i++;
 		while (nb[i])
 		{
-			tmp[arg->precision - ft_strlen(nb) + i] = nb[i + 1];
+			tmp[arg->precision + 1 - ft_strlen(nb) + i] = nb[i];
 			i++;
 		}
 		ft_strdel(&nb);
@@ -105,7 +104,7 @@ char 	*conversion_d2(t_format *arg, char *nb)
 
 void	conversion_d3(t_format *arg, char *nb, int len_nb, int i)
 {
-		arg->res = ft_memalloc(arg->width + 1);	
+		arg->res = ft_memalloc(arg->width + 2);	
 		if (ft_strchr(arg->option, '0') != 0)
 			ft_memset(arg->res, '0', arg->width);
 		else
