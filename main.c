dea5bcc45e99f4ea3bd9e5e0c8ced83e78bc2008
Sabri Makni <sabrimakni@yahoo.fr>
2018-06-27 00:35:45 +0200

    add tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 16:00:23 by smakni            #+#    #+#             */
-/*   Updated: 2018/06/26 23:57:54 by sabri            ###   ########.fr       */
+/*   Updated: 2018/06/27 00:32:23 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,26 @@
 int		main()
 {
 	char 	*str;
-	char 	*test;
+	char 	*test1;
+	char	*test2;
+	char	*test3;
 	int 	x;
 	char 	c;
 
-	x = 25;
+	x = -25;
 	c = '!';
 	str = "Sabri";
-	test = "j'ai %-14.2d ans et je m'appele %.8s%4c";
-	ft_printf(test, x, str, c);
-	printf(test, x, str, c);
+	test1 = "j'ai %-14.2d ans et je m'appele %.8s%4c\n";
+	test2 = "j'ai %-14.5d ans et je m'appele %.8s%4c\n";
+	test3 = "j'ai %+-0134.5d ans et je m'appele %.8s%4c\n";
+	ft_printf(">>>>>>>>>>>>>>>>>>>>ft_printf<<<<<<<<<<<<<<<<<<\n\n");
+	ft_printf(test1, x, str, c);
+	ft_printf(test2, x, str, c);
+	ft_printf(test3, x, str, c);
+	printf("\n>>>>>>>>>>>>>>>>>>>>>>printf<<<<<<<<<<<<<<<<<<<\n\n");
+	printf(test1, x, str, c);
+	printf(test2, x, str, c);
+	printf(test3, x, str, c);
 	//printf("j'ai %15.10d ans et je m'appele %2s%15c", x, str, c);
 	//ft_printf("%.2s", str);
 	//printf("%.2s", str);
