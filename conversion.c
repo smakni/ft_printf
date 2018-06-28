bf5da7b1179089e2ed6f1ba2dcb70f6efe198e19
Sabri Makni <sabrimakni@yahoo.fr>
2018-06-28 15:44:18 +0200

    conv_s opti

--- conversion.c
+++ conversion.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conversion.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/28 15:41:00 by sabri             #+#    #+#             */
+/*   Updated: 2018/06/28 15:43:02 by sabri            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 void	conversion_s(t_format *arg, va_list av)
@@ -13,36 +25,48 @@ void	conversion_s(t_format *arg, va_list av)
 	len = ft_strlen(tmp);
 	if (arg->precision < len)
 	{
-		while(tmp[i] && i < arg->precision)
-			i++;
-		tmp = ft_strsub_free(tmp, 0, i);
+		tmp = conversion_s1(arg, tmp);
 		len = ft_strlen(tmp);
-		i = 0;
 	}
 	if (arg->width > len)
-	{
-		arg->res = ft_memalloc(arg->width + 1);
-		if (ft_strchr(arg->option, '0') != 0 && ft_strchr(arg->option, '-') == 0)
-			ft_memset(arg->res, '0', arg->width);
-		else
-			ft_memset(arg->res, ' ', arg->width);
-		if (ft_strchr(arg->option, '-') != 0)
-			while (tmp[i])
-			{
-				arg->res[i] = tmp[i];
-				i++;
-			}
-		else
-			while (tmp[i])
-			{
-				arg->res[arg->width - len + i] = tmp[i];
-				i++;
-			}
-	}
+		arg->res = conversion_s2(arg, tmp, len, i);
 	else
 		arg->res = ft_strdup(tmp);
 	ft_strdel(&tmp);
 	ft_putstr(arg->res);
+}
+
+char	*conversion_s1(t_format *arg, char *tmp)
+{
+	int i;
+
+	i = 0;
+	while(tmp[i] && i < arg->precision)
+		i++;
+	tmp = ft_strsub_free(tmp, 0, i);
+	return (tmp);
+}
+
+char	*conversion_s2(t_format *arg, char *tmp, int len, int i)
+{
+	arg->res = ft_memalloc(arg->width + 1);
+	if (ft_strchr(arg->option, '0') != 0 && ft_strchr(arg->option, '-') == 0)
+		ft_memset(arg->res, '0', arg->width);
+	else
+		ft_memset(arg->res, ' ', arg->width);
+	if (ft_strchr(arg->option, '-') != 0)
+		while (tmp[i])
+		{
+			arg->res[i] = tmp[i];
+			i++;
+		}
+	else
+		while (tmp[i])
+		{
+			arg->res[arg->width - len + i] = tmp[i];
+			i++;
+		}
+	return (arg->res);
 }
 
 void	conversion_c(t_format *arg, va_list av)
