e3becfbceb7cd09521f493925d34884ca2dbe93b
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-07 07:20:47 +0200

    fixing conv_p

--- conversion_p.c
+++ conversion_p.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 19:22:09 by sabri             #+#    #+#             */
-/*   Updated: 2018/07/02 19:53:59 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/07 07:19:31 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,33 @@
 
 void	conversion_p(t_format *arg, va_list av)
 {
-	void	*adr;
+	void		*adr;
+	int			tmp;
+	long int	nb;
+	int			i;
 
+	i = 0;
+	(void)arg;
 	adr = va_arg(av, void*);
-	ft_putnbr(adr);
+	nb = (long int)adr;
+	while (nb != 0)
+	{
+		i++;
+		nb = nb / 16;
+	}
+	arg->res = ft_memalloc(i);
+	nb = (long int)adr;
+	while (nb != 0)
+	{
+		i--;
+		tmp = nb % 16;
+		if (tmp < 10)
+			tmp = tmp + 48;
+		else
+			tmp = tmp + 87;
+		arg->res[i] = tmp;
+		nb = nb / 16;
+	}
+	ft_putstr("0x");
+	ft_putstr(arg->res);
 }
