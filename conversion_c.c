91dbff67e230942e997e5b69e88fbcf87994ef6b
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-11 01:16:08 +0200

    fixed conv_c

--- conversion_c.c
+++ conversion_c.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 18:13:03 by sabri             #+#    #+#             */
-/*   Updated: 2018/07/10 13:16:51 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/10 14:34:21 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,16 @@ void	conversion_c(t_format *arg, va_list av)
 	{
 		if (!(arg->res = ft_memalloc(arg->width)))
 			return ;
-		ft_memset(arg->res, ' ', arg->width - 1);
+		if (ft_strchr(arg->option, '0') != 0)
+			ft_memset(arg->res, '0', arg->width - 1);
+		else
+			ft_memset(arg->res, ' ', arg->width - 1);
 		ft_putstr(arg->res);
+		arg->count = arg->width;
 		ft_strdel(&(arg)->res);
+		ft_putchar(va_arg(av, int));
+		return ;
 	}
-	arg->count = arg->width + 1;
+	arg->count = 1;
 	ft_putchar(va_arg(av, int));
 }
