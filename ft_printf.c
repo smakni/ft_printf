3dd3fab231ba207916f895f484092c7924234e5f
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-12 00:44:31 +0200

    add function ft_printf_param

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:45:26 by smakni            #+#    #+#             */
-/*   Updated: 2018/07/11 15:52:18 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/12 00:19:13 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,6 +65,53 @@ int		ft_printf(const char *format, ...)
 			ft_analyse(arg);
 			ft_conversion(arg, av);
 			//ft_aff_param(arg);
+			tmp = ft_strsub_free(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], '\0'));
+			i += arg->len;
+			ret += arg->count;
+			free_arg(arg);
+		}
+		else
+		{
+			ft_putchar(format[i]);
+			j++;
+		}
+		i++;
+	}
+	free(arg);
+	ft_strdel(&tmp);
+	va_end(av);
+	return (ret + j);
+}
+
+//-----------------------------------------------------------------------------------//
+
+int		ft_printf_param(const char *format, ...)
+{
+	va_list 	av;
+	char 		*tmp;
+	int 		i;
+	int			j;
+	int			ret;
+	t_format 	*arg;
+
+	arg = ft_memalloc(sizeof(t_format));
+	init_struc(arg);	
+	tmp = ft_strdup(format);
+	va_start(av, format);
+	i = 0;
+	j = 0;
+	ret = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			arg->type = check_conv(tmp);
+			if (arg->type == '%')
+				i++;
+			arg->str = ft_strsub(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], arg->type));
+			ft_analyse(arg);
+			ft_conversion(arg, av);
+			ft_aff_param(arg);
 			tmp = ft_strsub_free(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], '\0'));
 			i += arg->len;
 			ret += arg->count;
