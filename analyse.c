d29031294517798814834c7455ef13f3f1089d83
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-17 19:08:03 +0200

    add conv_X

--- analyse.c
+++ analyse.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/20 23:45:05 by sabri             #+#    #+#             */
-/*   Updated: 2018/07/11 01:43:18 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/16 18:20:13 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ void 	ft_conversion(t_format *arg, va_list av)
 		conversion_p(arg, av);
 	if (arg->type == 'x')
 		conversion_x(arg, av);
+	if (arg->type == 'X')
+		conversion_X(arg, av);
 	if (arg->type == 'o')
 		conversion_o(arg, av);
 	if (arg->type == '%')
