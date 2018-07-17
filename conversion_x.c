5b13dde182e8f42c492e5c752303217c496c7600
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-17 19:08:43 +0200

    add conv_X

--- conversion_x.c
+++ conversion_x.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 13:07:08 by sabri             #+#    #+#             */
-/*   Updated: 2018/07/11 00:51:30 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/14 23:30:25 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,37 @@ void	conversion_x(t_format *arg, va_list av)
 			tmp = tmp + 48;
 		else
 			tmp = tmp + 87;
+		arg->res[i] = tmp;
+		nb = nb / 16;
+	}
+	arg->count = ft_strlen(arg->res);
+	ft_putstr(arg->res);
+}
+
+void	conversion_X(t_format *arg, va_list av)
+{
+	unsigned int nb;
+	int tmp;
+	int i;
+
+	i = 0;
+	nb = va_arg(av, int);
+	tmp = nb;
+	while (nb != 0)
+	{
+		i++;
+		nb = nb / 16;
+	}
+	arg->res = ft_memalloc(i + 1);
+	nb = tmp;
+	while (nb != 0)
+	{
+		i--;
+		tmp = nb % 16;
+		if (tmp < 10)
+			tmp = tmp + 48;
+		else
+			tmp = tmp + 55;
 		arg->res[i] = tmp;
 		nb = nb / 16;
 	}
