a0827cd1d97ec36043a40b48ddb41ebd8b66a9a9
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-17 19:09:16 +0200

    add len_x and change ft_printf algo

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:45:26 by smakni            #+#    #+#             */
-/*   Updated: 2018/07/12 13:10:48 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/17 19:04:45 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,17 @@ void	free_arg(t_format *arg)
 	ft_strdel(&arg->res);
 }
 
+int		len_x(const char *str, int x, char c)
+{
+	int count;
+
+	count = 1;
+	while (str[x + count] && str[x + count] != c)
+		count++;
+	return(count);
+
+}
+
 int		ft_printf(const char *format, ...)
 {
 	va_list 	av;
@@ -45,6 +56,7 @@ int		ft_printf(const char *format, ...)
 	int 		i;
 	int			j;
 	int			ret;
+	int			len;
 	t_format 	*arg;
 
 	arg = ft_memalloc(sizeof(t_format));
@@ -54,62 +66,17 @@ int		ft_printf(const char *format, ...)
 	i = 0;
 	j = 0;
 	ret = 0;
+	len = 0;
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
-			arg->type = check_conv(tmp);
-			arg->str = ft_strsub(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], arg->type));
+			arg->type = check_conv(format, i);
+			len = len_x(format, i, arg->type);
+			arg->str = ft_strsub(format, i + 1, len);
 			ft_analyse(arg);
 			ft_conversion(arg, av);
-			tmp = ft_strsub_free(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], '\0'));
-			i += arg->len;
-			ret += arg->count;
-			free_arg(arg);
-		}
-		else
-		{
-			ft_putchar(format[i]);
-			j++;
-		}
-		i++;
-	}
-	free(arg);
-	ft_strdel(&tmp);
-	va_end(av);
-	return (ret + j);
-}
-
-//-----------------------------------------------------------------------------------//
-
-int		ft_printf_param(const char *format, ...)
-{
-	va_list 	av;
-	char 		*tmp;
-	int 		i;
-	int			j;
-	int			ret;
-	t_format 	*arg;
-
-	arg = ft_memalloc(sizeof(t_format));
-	init_struc(arg);	
-	tmp = ft_strdup(format);
-	va_start(av, format);
-	i = 0;
-	j = 0;
-	ret = 0;
-	while (format[i])
-	{
-		if (format[i] == '%')
-		{
-			arg->type = check_conv(tmp);
-			if (arg->type == '%')
-				i++;
-			arg->str = ft_strsub(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], arg->type));
-			ft_analyse(arg);
-			ft_conversion(arg, av);
-			ft_aff_param(arg);
-			tmp = ft_strsub_free(tmp, ft_strlen_c(tmp, format[i]) + 1, ft_strlen_from(tmp, format[i], '\0'));
+			//ft_aff_param(arg);
 			i += arg->len;
 			ret += arg->count;
 			free_arg(arg);
