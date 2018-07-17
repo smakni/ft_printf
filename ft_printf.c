df231b46c424a8144204650d56b928805ebb92dd
Sabri Makni <sabrimakni@yahoo.fr>
2018-07-17 20:33:25 +0200

    del tmp

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/08 14:45:26 by smakni            #+#    #+#             */
-/*   Updated: 2018/07/17 19:04:45 by sabri            ###   ########.fr       */
+/*   Updated: 2018/07/17 20:27:15 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,6 @@ int		len_x(const char *str, int x, char c)
 int		ft_printf(const char *format, ...)
 {
 	va_list 	av;
-	char 		*tmp;
 	int 		i;
 	int			j;
 	int			ret;
@@ -61,7 +60,6 @@ int		ft_printf(const char *format, ...)
 
 	arg = ft_memalloc(sizeof(t_format));
 	init_struc(arg);	
-	tmp = ft_strdup(format);
 	va_start(av, format);
 	i = 0;
 	j = 0;
@@ -76,9 +74,9 @@ int		ft_printf(const char *format, ...)
 			arg->str = ft_strsub(format, i + 1, len);
 			ft_analyse(arg);
 			ft_conversion(arg, av);
-			//ft_aff_param(arg);
 			i += arg->len;
 			ret += arg->count;
+			//ft_aff_param(arg);
 			free_arg(arg);
 		}
 		else
@@ -89,7 +87,6 @@ int		ft_printf(const char *format, ...)
 		i++;
 	}
 	free(arg);
-	ft_strdel(&tmp);
 	va_end(av);
 	return (ret + j);
 }
