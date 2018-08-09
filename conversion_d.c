880c40af847720fae8193828d75178824a28d9bc
Sabri MAKNI <smakni@e3r5p1.42.fr>
2018-08-09 17:50:15 +0200

    conv_x OK

--- conversion_d.c
+++ conversion_d.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/25 23:12:30 by sabri             #+#    #+#             */
-/*   Updated: 2018/08/09 12:35:31 by smakni           ###   ########.fr       */
+/*   Updated: 2018/08/09 17:46:31 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,10 @@ void	conversion_d(t_format *arg, va_list av)
 	nb = conversion_d0(arg, nb, av);
 	nb = conversion_d1(arg, nb);
 	check = conversion_null(arg, nb);
-	if (arg->precision >= (len_nb = ft_strlen(nb)))
+	if (arg->precision > (len_nb = ft_strlen(nb)))
 		nb = conversion_d2(arg, nb);
+	if (ft_strchr(arg->option, '#') != 0)
+		nb = conversion_x1(arg, nb);
 	if (arg->width > (len_nb = ft_strlen(nb)))
 		conversion_d3(arg, nb, i, len_nb);
 	else if (check == 0 || ((arg->type == 'o' || arg->type == 'O')
@@ -49,7 +51,6 @@ void	conversion_d(t_format *arg, va_list av)
 				&& ft_strchr(arg->option, '+') == 0 && arg->width > len_nb)
 		arg->res[0] = ' ';
 	arg->count = ft_strlen(arg->res);
-	conversion_x1(arg, nb);
 	ft_strdel(&nb);
 	ft_putstr(arg->res);
 }
@@ -112,16 +113,41 @@ char 	*conversion_d2(t_format *arg, char *nb)
 	return (nb);
 }
 
+char 	*conversion_d2x(t_format *arg, char *nb)
+{
+	int i;
+	char *tmp;
+
+	i = 0;
+	tmp = ft_memalloc(arg->precision + 3);
+	ft_memset(tmp, '0', arg->precision);
+	tmp[0] = '0';
+	if (arg->type == 'x')
+		tmp[1] = 'x';
+	else
+		tmp[1] = 'X';
+	while (nb[i])
+	{
+		tmp[arg->precision - ft_strlen(nb) + i + 2] = nb[i];
+		i++;
+	}
+	ft_strdel(&nb);
+	nb = tmp;
+	return (nb);
+}
+
 void	conversion_d3(t_format *arg, char *nb, int i, int len_nb)
 {
 	conversion_d4(arg);
+	if (len_nb == 1 && nb[0] == '0' && arg->type != 'o' && arg->type != 'O')
+		return ;
 	if (ft_strchr(arg->option, '-') != 0)
 		while (nb[i])
 		{
 			arg->res[i] = nb[i];
 			i++;
 		}
-	else if (ft_isdigit(nb[0]) == 0 && ft_strchr(arg->option, '0') != 0 
+	else if (ft_isalnum(nb[0]) == 0 && ft_strchr(arg->option, '0') != 0
 			&& ft_strchr(arg->option, '-') == 0 && arg->precision == 0) 
 	{
 		arg->res[0] = nb[0];
@@ -132,6 +158,19 @@ void	conversion_d3(t_format *arg, char *nb, int i, int len_nb)
 			i++;
 		}
 	}
+	else if (ft_strchr(arg->option, '0') != 0 && ft_strchr(arg->option, '#')
+			&& ft_strchr(arg->str, '.') == 0
+			&& (arg->type == 'x' || arg->type == 'X')) 
+	{
+		arg->res[0] = nb[0];
+		arg->res[1] = nb[1];
+		i += 2;
+		while (nb[i])
+		{
+			arg->res[arg->width - len_nb + i] = nb[i];
+			i++;
+		}
+	}	
 	else
 		while (nb[i])
 		{
@@ -172,20 +211,36 @@ int		conversion_null(t_format *arg, char *nb)
 	return (check);
 }
 
-void	conversion_x1(t_format *arg, char *nb)
+char	*conversion_x1(t_format *arg, char *nb)
 {
+	char *x;
+	char *X;
+
+	if (nb[0] == '0' && ft_strlen(nb) == 1)
+		return (ft_strdup("0"));
+	x = NULL;
+	X = NULL;
 	if (arg->type == 'x' && ft_strchr(arg->option, '#') != 0)
 	{
-		ft_putstr("0x");
-		arg->count += 2;
+		x = ft_memalloc(3 + ft_strlen(nb));
+		x[0] = '0';
+		x[1] = 'x';
+		x[2] = '\0';
+		x = ft_strcat(x, nb);
+		ft_strdel(&nb);
+		nb = ft_strdup(x);
+		ft_strdel(&x);
 	}
 	else if (arg->type == 'X' && ft_strchr(arg->option, '#') != 0)
 	{
-		ft_putstr("0X");
-		arg->count += 2;
+		X = ft_memalloc(3 + ft_strlen(nb));
+		X[0] = '0';
+		X[1] = 'X';
+		X[2] = '\0';
+		X = ft_strcat(X, nb);
+		ft_strdel(&nb);
+		nb = ft_strdup(X);
+		ft_strdel(&X);
 	}
-	else if ((arg->type == 'o' || arg->type == 'O') 
-				&& ft_strchr(arg->option, '#') != 0
-				&& nb[0] != '0')
-		arg->res[0] = '0';
+	return (nb);
 }
