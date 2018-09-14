361ddf4f50efe830451537e3992d6c57973a29f5
Sabri MAKNI <smakni@e3r2p12.42.fr>
2018-09-14 16:38:29 +0200

    add: Write for 4 bytes

--- ft_putchar_printf.c
+++ ft_putchar_printf.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/13 16:48:11 by smakni            #+#    #+#             */
-/*   Updated: 2018/09/13 17:18:47 by smakni           ###   ########.fr       */
+/*   Updated: 2018/09/14 16:19:17 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void    ft_putchar_printf(wint_t c, t_format *arg)
 {
+    if (c < 0)
+    {
+        arg->count = -1;
+        return ;
+    }
     if (c <= 127)
         ft_putchar1(c);
     else if (c <= 2047)
@@ -26,6 +31,14 @@ void    ft_putchar_printf(wint_t c, t_format *arg)
     {   
         arg->count += 2;
         ft_putchar3(c);
+    }
+    else if ((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605) 
+                || (c >= 196607 && c <= 983037) 
+                || (c >= 983040 && c <= 1048573)
+                || (c >= 1048576 && c <= 1114109))
+    {
+        arg->count += 3;
+        ft_putchar4(c);
     }
     else
         arg->count = -1;
