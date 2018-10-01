93f84d93dba733db5b1a06caa05eb4b7de809e18
smakni <sabrimakni@gmail.com>
2018-10-01 12:13:20 +0200

    upd: unicode

--- conversion_c.c
+++ conversion_c.c
@@ -16,26 +16,23 @@ static int check_c(wint_t c)
 {
 	if (c <= 127)
         return (1);
-    else if (c <= 2047)
+    else if (c <= 2047 && MB_CUR_MAX >=1)
 		return (2);
-    else if ((c <= 55295 && c >= 57344)
-             || (c <= 64975 && c >= 65007) || c <= 65533)
+	else if ((c <= 55295 || (c >= 57344 && c <= 64975) 
+			|| (c >= 65008 && c <= 65533)) && MB_CUR_MAX >= 2)
 		return (3);
-    else if ((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605)
-                || (c >= 196607 && c <= 983037)
-                || (c >= 983040 && c <= 1048573)
-                || (c >= 1048576 && c <= 1114109))
+    else if (((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605)
+	    	|| (c >= 917504 && c <= 983037)
+			|| (c >= 983040 && c <= 1048573)
+			|| (c >= 1048576 && c <= 1114109)) && MB_CUR_MAX >= 3)
 		return (4);
     else
-		return (0);
+		return (-1);
 }
 
-static void	conversion_c1(t_format *arg, va_list av)
+static void	conversion_c1(t_format *arg, wint_t c)
 {
-	if (arg->type == 'c' && (ft_strchr(arg->size, 'l') == 0))
-		ft_putchar_printf(va_arg(av, int), arg);
-	else
-		ft_putchar_printf(va_arg(av, wint_t), arg);
+	ft_putchar_printf(c, arg);
 	if ((arg->res = ft_memalloc(arg->width - arg->count - 1)) == NULL)
 			return ;
 	ft_memset(arg->res, ' ', arg->width - arg->count - 1);
@@ -47,39 +44,34 @@ static void	conversion_c1(t_format *arg, va_list av)
 
 }
 
-static void	conversion_c2(t_format *arg, va_list av)
+static void	conversion_c2(t_format *arg, wint_t c, int check)
 {
-	wint_t 	c;
-	int		check;
-
-	if (arg->type == 'c' && (ft_strchr(arg->size, 'l') == 0))
-		c = va_arg(av, int);
-	else
-		c = va_arg(av, wint_t);
-	check = check_c(c);
 	if ((arg->res = ft_memalloc(arg->width - check + 1)) == NULL)
 			return ;
 	ft_memset(arg->res, ' ', arg->width - check);
 	ft_putstr_printf(arg->res, arg);
 	arg->count = arg->width - check + 1; 
-	if (arg->type == 'c' && (ft_strchr(arg->size, 'l') == 0))
-		ft_putchar_printf(c, arg);
-	else
-		ft_putchar_printf(c, arg);
+	ft_putchar_printf(c, arg);
 }
 
 void	conversion_c(t_format *arg, va_list av)
 {
+	wint_t 	c;
+	int		check;
+
+	c = va_arg(av, wint_t);
+	if ((check = check_c(c)) == -1)
+	{
+		arg->check = -1;
+		return ;
+	}
 	if (arg->width > 1) 
 	{
 		if (ft_strchr(arg->option, '-') != 0)
-			return (conversion_c1(arg, av));
+			return (conversion_c1(arg, c));
 		else
-			return (conversion_c2(arg, av));
+			return (conversion_c2(arg, c, check));
 	}
 	arg->count = 1;
-	if (arg->type == 'c' && (ft_strchr(arg->size, 'l') == 0))
-		ft_putchar_printf(va_arg(av, int), arg);
-	else
-		ft_putchar_printf(va_arg(av, wint_t), arg);
+	ft_putchar_printf(c, arg);
 }
