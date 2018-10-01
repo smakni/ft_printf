179eb21b3fcb4592b453f136177c58c5a3a818b2
smakni <sabrimakni@gmail.com>
2018-10-01 12:15:21 +0200

    upd: unicode

--- ft_putchar_printf.c
+++ ft_putchar_printf.c
@@ -13,7 +13,7 @@
 #include "ft_printf.h"
 
 void    ft_putchar_printf(wint_t c, t_format *arg)
-{
+{	 
     if (c <= 127)
         ft_putchar1(c);
     else if (c <= 2047)
@@ -21,16 +21,16 @@ void    ft_putchar_printf(wint_t c, t_format *arg)
         arg->count++;
         ft_putchar2(c);
     }
-    else if ((c <= 55295 && c >= 57344)
-             || (c <= 64975 && c >= 65007) || c <= 65533)
+	else if (c <= 55295 || (c >= 57344 && c <= 64975) 
+			|| (c >= 65008 && c <= 65533))
     {   
         arg->count += 2;
         ft_putchar3(c);
     }
-    else if ((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605) 
-                || (c >= 196607 && c <= 983037) 
-                || (c >= 983040 && c <= 1048573)
-                || (c >= 1048576 && c <= 1114109))
+    else if ((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605)
+	    	|| (c >= 917504 && c <= 983037)
+			|| (c >= 983040 && c <= 1048573)
+			|| (c >= 1048576 && c <= 1114109))
     {
         arg->count += 3;
         ft_putchar4(c);
