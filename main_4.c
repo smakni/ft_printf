397903d4a55b2ea54f07dd48606d3499c0777051
smakni <sabrimakni@gmail.com>
2018-10-16 23:52:04 +0200

    add tests

--- main_4.c
+++ main_4.c
@@ -4,5 +4,7 @@ int main()
 {
 	ft_printf("test(1)%d%d%d%dfin\n", 1, 2, 3, 4);
 	ft_printf("test(2)%d%d%d%sfin\n", 1, 2, 3, "et");
+	ft_printf("test(3)%%%%%d%d%d%sfin\n", 1, 2, 3, "et");
+	ft_printf("test(4)%c", 245);
 	return (0);
 }
