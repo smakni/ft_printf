fa5285cb1c4639f89c7f246f8d73d374777dc0d7
Sabri MAKNI <smakni@e2r3p20.42.fr>
2018-10-17 18:16:08 +0200

    add tests

--- main_4.c
+++ main_4.c
@@ -3,9 +3,9 @@
 int main()
 {
     setlocale(LC_ALL, "");
-	ft_printf("test(1)%d%d%d%dfin\n", 1, 2, 3, 4);
-	ft_printf("test(2)%d%d%d%sfin\n", 1, 2, 3, "et");
-	ft_printf("test(3)%%%%%d%d%d%sfin\n", 1, 2, 3, "et");
-	ft_printf("test(4)%c", 24855);
+	ft_printf("--------------\n");
+	(ft_printf("MYTEST : hello ca%----4c %1c va %10c%-c ??", '\0', '\n', (char)564, 0));
+	ft_printf("\n--------------\n");
+	(printf("CPTEST : hello ca%----4c %1c va %10c%-c ??", '\0', '\n', (char)564, 0));
 	return (0);
 }
