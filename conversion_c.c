ea2a0e2974d55744496d75d7e8016d727caee9db
Sabri MAKNI <smakni@e2r3p20.42.fr>
2018-10-17 18:12:16 +0200

    upd: fixing conv_c

--- conversion_c.c
+++ conversion_c.c
@@ -6,50 +6,51 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 18:13:03 by sabri             #+#    #+#             */
-/*   Updated: 2018/10/17 15:36:56 by smakni           ###   ########.fr       */
+/*   Updated: 2018/10/17 18:10:45 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int check_c(wchar_t c)
+static int check_c(t_format *arg, wchar_t c)
 {
 	if (c <= 127)
 		return (1);
-    else if (c <= 2047 && MB_CUR_MAX >=2)
-		return (2);
-	else if ((c <= 55295 || (c >= 57344 && c <= 64975) 
-			|| (c >= 65008 && c <= 65533)) && MB_CUR_MAX >= 3)
-		return (3);
-    else if (((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605)
-	    	|| (c >= 917504 && c <= 983037)
-			|| (c >= 983040 && c <= 1048573)
-			|| (c >= 1048576 && c <= 1114109)) && MB_CUR_MAX >= 4)
-		return (4);
-    else
-		return (-1);
+	else if ((arg->type == 'c' && ft_strcmp(arg->size, "l") == 0)
+				|| arg->type == 'C')
+	{
+    	 if (c <= 2047 && MB_CUR_MAX >=2)
+			return (2);
+		else if ((c <= 55295 || (c >= 57344 && c <= 64975) 
+				|| (c >= 65008 && c <= 65533)) && MB_CUR_MAX >= 3)
+			return (3);
+   		 else if (((c >= 65536 && c <= 131069) || (c >= 131072 && c <= 196605)
+		    	|| (c >= 917504 && c <= 983037)
+				|| (c >= 983040 && c <= 1048573)
+				|| (c >= 1048576 && c <= 1114109)) && MB_CUR_MAX >= 4)
+			return (4);
+	}
+	return (1);
 }
 
-static void	conversion_c1(t_format *arg, wchar_t c, int check, char *str)
+static void	conversion_c1(t_format *arg, int check)
 {
-	(void)check;
-	ft_putchar_printf(c, arg, str);
-	if ((arg->res = ft_memalloc(arg->width - arg->count - 1)) == NULL)
-			return ;
-	ft_memset(arg->res, ' ', arg->width - arg->count - 1);
-	if (arg->count < arg->width)
-		arg->count = arg->width;
-	else
-		arg->count += arg->width;
-}
+	char *tmp;
 
-static void	conversion_c2(t_format *arg, wchar_t c, int check, char *str)
-{
-	if ((arg->res = ft_memalloc(arg->width - check + 1)) == NULL)
+	if ((tmp = ft_memalloc(arg->width - check)) == NULL)
 			return ;
-	ft_memset(arg->res, ' ', arg->width - check);
-	arg->count = arg->width - check + 1; 
-	ft_putchar_printf(c, arg, str);
+	ft_memset(tmp, ' ', arg->width - check);
+	arg->res = ft_strjoin(arg->res, tmp); 
+}
+static void	conversion_c2(t_format *arg, int check)
+{
+	char *tmp;
+
+	if ((tmp = ft_memalloc(arg->width - check + 1)) == NULL)
+			return ;
+	ft_memset(tmp, ' ', arg->width - check);
+	arg->res= ft_strjoin(tmp, arg->res);
+	
 }
 
 void	conversion_c(t_format *arg, va_list av)
@@ -59,16 +60,19 @@ void	conversion_c(t_format *arg, va_list av)
 	int			check;
 
 	c = va_arg(av, wchar_t);
-	if ((check = check_c(c)) != -1)
+	if (c == '\0')
+		arg->count += 1;
+	if ((check = check_c(arg, c)) != -1)
 		str = ft_memalloc(check);
 	else
 		return ;
+	arg->res = ft_strdup(ft_putchar_printf(c, str, check));
 	if (arg->width > 1) 
 	{
 		if (ft_strchr(arg->option, '-') != 0)
-			return (conversion_c1(arg, c, check, str));
+			conversion_c1(arg, check);
 		else
-			return (conversion_c2(arg, c, check, str));
+			conversion_c2(arg, check);
 	}
-	arg->res = ft_strdup(ft_putchar_printf(c, arg, str));
+	arg->count += ft_strlen(arg->res);
 }
