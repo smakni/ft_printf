8edb1bf2fd8e1f727cb7c4857e16a12960a891a1
Sabri MAKNI <sabrimakni@gmail.com>
2018-10-19 17:45:02 +0200

    add test

--- main_2.c
+++ main_2.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/14 11:55:45 by smakni            #+#    #+#             */
-/*   Updated: 2018/10/19 15:47:07 by smakni           ###   ########.fr       */
+/*   Updated: 2018/10/19 17:43:26 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,19 @@ int     main()
 {
     int x;
     char y;
+	wchar_t s[4];
+
 	setlocale(LC_ALL, "");
-	x = ft_printf("%-12i", 0);
+	s[0] = 0x53;
+	s[1] = 0x3abc;
+	s[2] = 0x81000;
+	s[3] = '\0';
+	//x = ft_printf("%-12i", 0); error
+	x  = ft_printf("MOI%S", s);
+	ft_printf("\n");
+	y = printf("CP%S", s);
+	ft_putnbr(x);
+	ft_putchar('/');
+	ft_putnbr(y);
 
 }
