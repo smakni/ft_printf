bed0f023eafcdece4691d2d502ebf385c5c41a5f
smakni <sabrimakni@gmail.com>
2018-10-29 20:44:45 +0100

    add: ft_itoa_sit

---
+++ libft/ft_itoa_sit.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_sit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/23 15:03:20 by smakni            #+#    #+#             */
+/*   Updated: 2018/10/25 12:28:24 by smakni           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t		ft_size(size_t n)
+{
+	size_t	size;
+
+	size = 0;
+	if (n == 0)
+		return (1);
+	while (n != 0)
+	{
+		n = n / 10;
+		size++;
+	}
+	return (size);
+}
+
+char				*ft_itoa_sit(ssize_t n)
+{
+	char	*str;
+	size_t	size;
+
+	size = ft_size(n);
+	if ((str = malloc(sizeof(char) * (size + 1))) == NULL)
+		return (0);
+	str[size] = '\0';
+	size--;
+	while (size + 1 > 0)
+	{
+		str[size] = (n % 10) + '0';
+		n /= 10;
+		size--;
+	}
+	return (str);
+}
