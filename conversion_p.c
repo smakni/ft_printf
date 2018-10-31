1f4b8eae75eb78c4c6338cde39670889a2595b62
Sabri MAKNI <sabrimakni@gmail.com>
2018-10-31 18:58:12 +0100

    upd

--- conversion_p.c
+++ conversion_p.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/02 19:22:09 by sabri             #+#    #+#             */
-/*   Updated: 2018/10/24 16:37:59 by smakni           ###   ########.fr       */
+/*   Updated: 2018/10/31 17:43:00 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,11 @@ void	conversion_p(t_format *arg, va_list av)
 	if (arg->width > (int)ft_strlen(str_tmp))
 	{
 		arg->res = ft_memalloc(arg->width - (int)ft_strlen(str_tmp));
-		ft_memset(arg->res, ' ', arg->width - (int)ft_strlen(str_tmp));
-		if (ft_strchr(arg->option, '-') != 0)
+		if (ft_strchr(arg->option, '0') != 0 && ft_strchr(arg->option, '-') == 0)
+			ft_memset(arg->res, '0', arg->width - (int)ft_strlen(str_tmp));
+		else
+			ft_memset(arg->res, ' ', arg->width - (int)ft_strlen(str_tmp));
+		if (ft_strchr(arg->option, '0') != 0 || ft_strchr(arg->option, '-') != 0)
 			arg->res = ft_strjoin(str_tmp, arg->res);
 		else
 			arg->res = ft_strjoin(arg->res, str_tmp);
