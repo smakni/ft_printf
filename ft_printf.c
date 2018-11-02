d265c0471159c03d857deb02de01077ce5eac04a
smakni <sabrimakni@gmail.com>
2018-11-02 19:40:54 +0100

    upd: mod algo

--- ft_printf.c
+++ ft_printf.c
@@ -51,42 +51,15 @@ int		len_x(const char *str, int x, char c)
 	return (count);
 }
 
-void	case_3(t_format *arg, t_control *ctr)
-{
-	char *tmp;
-	char *tmp2;
-
-	tmp = NULL;
-	tmp2 = NULL;
-	arg->width = check_widht(arg->str);
-	tmp = ft_strsub(arg->str, ctr->len, 1);
-	if (arg->width > 1)
-	{
-		tmp2 = ft_memalloc(arg->width + 1);
-		ft_memset(tmp2, ' ', arg->width);
-		tmp = ft_strjoin(tmp2, tmp);
-	}
-	ctr->result = ft_strjoin(ctr->result, tmp);
-	if (arg->width > 1)
-		ctr->ret += arg->width - 1;
-	else
-		ctr->ret += 1;
-	ctr->i += ctr->len;
-	ft_strdel(&tmp);
-	ft_strdel(&tmp2);
-	free_arg(arg);
-}
-
 int		case_1(t_format *arg, t_control *ctr, const char *format, va_list av)
 {
-	arg->type = check_conv(format, ctr->i);
-	ctr->len = len_x(format, ctr->i, arg->type);
-	arg->str = ft_strsub(format, ctr->i + 1, ctr->len);
-	if (ft_strchr(CONV, arg->type) == 0)
+	if ((arg->type = check_conv(format, ctr->i)) == '0')
 	{	
-		case_3(arg, ctr);
+		ctr->i += len_x(format, ctr->i, '\0');
 		return (0);
 	}
+	ctr->len = len_x(format, ctr->i, arg->type);
+	arg->str = ft_strsub(format, ctr->i + 1, ctr->len);
 	ft_analyse(arg);
 	ft_conversion(arg, av);
 	if (arg->check == -1)
