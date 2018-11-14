fdcac7f9ecc5a6445c309bbae298dc85c112e751
smakni <sabrimakni@gmail.com>
2018-11-14 16:25:29 +0100

    upd: fixed conv_f

--- conversion_f.c
+++ conversion_f.c
@@ -12,45 +12,34 @@
 
 #include "ft_printf.h"
 
+static	int	get_precision(t_format *arg)
+{
+	if (ft_strchr(arg->str, '.') == 0)
+		return (6);
+	else
+		return (arg->precision);
+}
+
+static	char	*f0(int precision)
+{
+	char *tmp;
+
+	if (precision == 0)
+		return (ft_strdup("0"));
+	tmp = ft_memalloc(precision + 1);
+	ft_memset(tmp, '0', precision);
+	tmp = ft_strjoin_free("0.", tmp);
+	return (tmp);
+} 
+
 static	char	*around(double n, int precision ,char c)
 {
-	if (n == 0)
-		return (ft_strdup("0.00000"));
-	else if (precision != 0)
+	if (precision != 0)
 		return (ft_strjoin_free1(ft_itoa_lli(n), "."));
 	else if (c > '4')
 		return (ft_itoa_lli(n + 1));
 	else
 		return (ft_itoa_lli(n));
-}
-
-static	char	*ft_itoa_f(t_format *arg, va_list av)
-{
-	char	*tmp;
-	char	*tmp2;
-	int		precision;
-	double	n;
-	int		s;
-
-	n = va_arg(av, double);
-	tmp = NULL;
-	s = 1;
-	if (ft_strchr(arg->str, '.') == 0)
-		precision = 6;
-	else
-		precision = arg->precision;
-	if (n < 0)
-		s = -1;
-	tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision + 1)));
-	tmp = around(n, precision, tmp2[precision]);
-	if (tmp2[precision] > '4')
-	{
-		ft_strdel(&tmp2);
-		tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision)) + 1);
-	}
-	tmp = ft_memjoin(tmp, tmp2, ft_strlen(tmp), precision);
-	ft_strdel(&tmp2);
-	return (tmp);
 }
 
 static	char	*around_bf(long double n, int precision ,char c)
@@ -65,21 +54,44 @@ static	char	*around_bf(long double n, int precision ,char c)
 		return (ft_itoa_lli(n));
 }
 
-static	char	*ft_itoa_bf(t_format *arg, va_list av)
+char	*ft_itoa_f(t_format *arg, double n)
+{
+	char	*tmp;
+	char	*tmp2;
+	int		precision;
+	int		s;
+
+	tmp = NULL;
+	s = 1;
+	precision = get_precision(arg);
+	if (n == 0)
+		return (f0(precision));
+	if (n < 0)
+		s = -1;
+	tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision + 1)));
+	tmp = around(n, precision, tmp2[precision]);
+	if (tmp2[precision] > '4')
+	{
+		ft_strdel(&tmp2);
+		tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision)) + 1);
+	}
+	tmp = ft_memjoin(tmp, tmp2, ft_strlen(tmp), precision);
+	ft_strdel(&tmp2);
+	return (tmp);
+}
+
+char			*ft_itoa_bf(t_format *arg, long double n)
 {
 	char		*tmp;
 	char		*tmp2;
 	int			precision;
-	long double	n;
 	int			s;
 
-	n = va_arg(av, long double);
 	tmp = NULL;
 	s = 1;
-	if (ft_strchr(arg->str, '.') == 0)
-		precision = 6;
-	else
-		precision = arg->precision;
+	precision = get_precision(arg);
+	if (n == 0)
+		return (f0(precision));
 	if (n < 0)
 		s = -1;
 	tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision + 1)));
@@ -94,10 +106,3 @@ static	char	*ft_itoa_bf(t_format *arg, va_list av)
 	return (tmp);
 }
 
-char			*conversion_f(t_format *arg, va_list av)
-{
-	if (ft_strchr(arg->str, 'L') != 0)
-		return (ft_itoa_bf(arg, av));
-	else
-		return (ft_itoa_f(arg, av));
-}
