e3e6a86703b8eee4afb84493ad4125aad27f2ec4
Sabri MAKNI <sabrimakni@gmail.com>
2018-11-17 19:30:04 +0100

    upd: fixing conv_f

--- printf/conversion_f.c
+++ printf/conversion_f.c
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 11:02:54 by smakni            #+#    #+#             */
-/*   Updated: 2018/11/17 15:23:38 by smakni           ###   ########.fr       */
+/*   Updated: 2018/11/17 19:28:54 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static	int		get_precision(t_format *arg)
 		return (arg->precision);
 }
 
-static	char	*around(double n, int precision, char c)
+static	char	*int_part(double n, int precision, char c)
 {
 	if (precision != 0)
 		return (ft_strjoin_free1(ft_itoa_lli(n), "."));
@@ -30,7 +30,7 @@ static	char	*around(double n, int precision, char c)
 		return (ft_itoa_lli(n));
 }
 
-static	char	*around_bf(long double n, int precision, char c)
+static	char	*int_part_bf(long double n, int precision, char c)
 {
 	if (n == 0)
 		return (ft_strdup("0.00000"));
@@ -42,28 +42,86 @@ static	char	*around_bf(long double n, int precision, char c)
 		return (ft_itoa_lli(n));
 }
 
+static	char	*floating_part(int precision, double n)
+{
+	char		*tmp;
+	double	x;
+	int			i;
+	int			stop;
+
+	tmp = "";
+	if (n < 0)
+		x = -n;
+	else
+		x = n;
+	i = 1;
+	stop = 0;
+	printf("X = %F\n", x);
+	while (stop < precision - 1)
+	{
+		x = (x - (long long)x) * ft_power(10, (i));
+		printf("STOP = %d\n", stop);
+		printf("i = %d\n", i);
+		printf("X = %f\n", x);
+		printf("TMP2 = %s\n", tmp);
+		tmp = ft_strjoin(tmp, ft_itoa_lli(x));
+		stop += i;
+	}
+	x = (x - (long long)x) * ft_power(10, (i));
+	if ((x - (long long)x) * ft_power(10, (i)) > 4)
+		tmp = ft_strjoin(tmp, ft_itoa_lli(x + 1));
+	else
+		tmp = ft_strjoin(tmp, ft_itoa_lli(x));
+	printf("tmpfinal = %s\n", tmp);
+	return (tmp);
+}
+
+static	char	*floating_part_bf(int precision, long double n)
+{
+	char		*tmp;
+	long double	x;
+	int			i;
+	int			stop;
+
+	if (n < 0)
+		x = -n;
+	else
+		x = n;
+	tmp = "";
+	i = 1;
+	stop = 0;
+	//printf("X = %.20LF\n", x);
+	while (stop < precision - 1)
+	{
+		x = (x - (long long)x) * ft_power(10, (i));
+		/*printf("STOP = %d\n", stop);
+		printf("i = %d\n", i);
+		printf("X = %Lf\n", x);
+		printf("TMP2 = %s\n", tmp);*/
+		tmp = ft_strjoin(tmp, ft_itoa_lli(x));
+		stop += i;
+	}
+	x = (x - (long long)x) * ft_power(10, (i));
+	if ((x - (long long)x) * ft_power(10, (i)) > 4)
+		tmp = ft_strjoin(tmp, ft_itoa_lli(x + 1));
+	else
+		tmp = ft_strjoin(tmp, ft_itoa_lli(x));
+	//printf("tmpfinal = %s\n", tmp);
+	return (tmp);
+}
+
 char			*ft_itoa_f(t_format *arg, double n)
 {
 	char	*tmp;
 	char	*tmp2;
 	int		precision;
-	int		s;
 
 	tmp = NULL;
-	s = 1;
 	precision = get_precision(arg);
 	if (n == 0)
 		return (f0(precision));
-	if (n < 0)
-		s = -1;
-	tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision + 1)));
-	tmp = around(n, precision, tmp2[precision]);
-	if (tmp2[precision] > '4')
-	{
-		ft_strdel(&tmp2);
-		tmp2 = ft_itoa_lli((n - (long long)n) * s *
-								ft_power(10, (precision)) + 1);
-	}
+	tmp2 = floating_part(precision, n);		
+	tmp = int_part(n, precision, tmp2[precision]);
 	tmp = ft_memjoin(tmp, tmp2, ft_strlen(tmp), precision);
 	ft_strdel(&tmp2);
 	return (tmp);
@@ -74,23 +132,13 @@ char			*ft_itoa_bf(t_format *arg, long double n)
 	char		*tmp;
 	char		*tmp2;
 	int			precision;
-	int			s;
 
 	tmp = NULL;
-	s = 1;
 	precision = get_precision(arg);
 	if (n == 0)
 		return (f0(precision));
-	if (n < 0)
-		s = -1;
-	tmp2 = ft_itoa_lli((n - (long long)n) * s * ft_power(10, (precision + 1)));
-	tmp = around_bf(n, precision, tmp2[precision]);
-	if (tmp2[precision] > '4')
-	{
-		ft_strdel(&tmp2);
-		tmp2 = ft_itoa_lli((n - (long long)n) * s *
-								ft_power(10, (precision)) + 1);
-	}
+	tmp2 = floating_part_bf(precision, n);		
+	tmp = int_part_bf(n, precision, tmp2[precision]);
 	tmp = ft_memjoin(tmp, tmp2, ft_strlen(tmp), precision);
 	ft_strdel(&tmp2);
 	return (tmp);
